061745b610f78118ab493f66bff3fbdb47980023
Miguel Fernandez-Cortizas <43600658+miferco97@users.noreply.github.com>
2025-12-03 08:20:30 +0000

    day1part2

--- day01/main.py
+++ day01/main.py
@@ -7,7 +7,8 @@ def read_input(file_path):
 if __name__ == "__main__":
     # part 1 
 
-    input_data = read_input('input.txt')
+    # input_data = read_input('inputs/input.txt')
+    input_data = read_input('inputs/example1.txt')
 
     code=0
     value = 50
@@ -31,21 +32,27 @@ if __name__ == "__main__":
 
     code=0
     value = 50
+    n_zeros_during_rotation = 0
     for line in input_data:
         action = line[0]
         number = line[1:]
-        # print(f'{action} : {number}')
+        # print(f'{value=} {action} : {number}')
         if action == 'R':
             value += int(number)
         elif action == 'L':
             value -= int(number)
         else:
             raise Exception('no valid action')
-        value = value % 100
-        if value == 0:
-            code += 1
+        
+        quotient = value // 100
+        remainder = value % 100
 
-    print(f'Part1 {code=}')
+        # print(f'{quotient=} {remainder=}')
+        value = value % 100
+
+        n_zeros_during_rotation += abs(quotient)
+
+    print(f'Part2 code={n_zeros_during_rotation}')
 
 
 
